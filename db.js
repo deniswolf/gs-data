30f28bd692273a99ab8af1546696f7bea2043790
Denis Wolf <dear.mr.dw@gmail.com>
2015-03-24 22:08:19 +0100

    users 'db' module with search

---
+++ db.js
@@ -0,0 +1,21 @@
+var users = require('./users.db.json');
+var possibleParams = ['id', 'name', 'role', 'name'];
+
+function findByKey(collection, key, value) {
+	return collection.filter(function(item) {
+		return item[key] === value;
+	});
+}
+
+// note: the simplest/fastest implementation, IRL it would be outsourced to DB to handle
+function queryUsers(params) {
+	var result = users;
+
+	possibleParams.forEach(function(key) {
+		if (params[key] !== undefined) result = findByKey(result, key, params[key]);
+	});
+
+	return result;
+}
+
+module.exports = queryUsers;
