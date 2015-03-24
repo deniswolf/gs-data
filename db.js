2e7c075eb97dea94a89f7b24fdd7ff4f843eb0d4
Denis Wolf <dear.mr.dw@gmail.com>
2015-03-25 00:36:22 +0100

    promisified db stub

--- db.js
+++ db.js
@@ -1,21 +1,25 @@
 var users = require('./users.db.json');
 var possibleParams = ['id', 'name', 'role', 'name', 'created_at'];
+var Promise = require('bluebird');
 
 function findByKey(collection, key, value) {
-	return collection.filter(function(item) {
-		return item[key] === value;
-	});
+  return collection.filter(function(item) {
+    return item[key] === value;
+  });
 }
 
 // note: the simplest/fastest implementation, IRL it would be outsourced to DB to handle
 function queryUsers(params) {
-	var result = users;
+  return new Promise(function(res, rej) {
+    var result = users;
 
-	possibleParams.forEach(function(key) {
-		if (params[key] !== undefined) result = findByKey(result, key, params[key]);
-	});
+    possibleParams.forEach(function(key) {
+      if (params[key] !== undefined) result = findByKey(result, key, params[key]);
+    });
 
-	return result;
+    return res(result);
+  }
+	);
 }
 
 module.exports = queryUsers;
