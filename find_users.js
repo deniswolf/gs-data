68ae7a2f0d1bc57260201fe714e616103a9adca8
Denis Wolf <dear.mr.dw@gmail.com>
2015-03-24 23:21:20 +0100

    auto re-indented code, no changes

--- find_users.js
+++ find_users.js
@@ -2,24 +2,24 @@ var queryUsers = require('./db');
 var possibleParams = ['id', 'name', 'role', 'name', 'created_at'];
 
 function validateParamsNames(params) {
-  var keys = Object.keys(params);
-  if (keys.length === 0) return 'search params could not be empty';
-  for (var i = 0; i < keys.length; i++) {
-    if (possibleParams.indexOf(keys[i]) === -1) return 'invalid query parameter: ' + keys[i];
-  }
-  return true;
+	var keys = Object.keys(params);
+	if (keys.length === 0) return 'search params could not be empty';
+	for (var i = 0; i < keys.length; i++) {
+		if (possibleParams.indexOf(keys[i]) === -1) return 'invalid query parameter: ' + keys[i];
+	}
+	return true;
 }
 
 module.exports = function(req, res) {
-  var params = req.query;
-  var namesValidation = validateParamsNames(params);
+	var params = req.query;
+	var namesValidation = validateParamsNames(params);
 
-  if (namesValidation !== true) {
-    res.status(400).json({
-      error: namesValidation
-    });
-  } else {
-    var result = queryUsers(params);
-    res.json(result);
-  }
+	if (namesValidation !== true) {
+		res.status(400).json({
+			error: namesValidation
+		});
+	} else {
+		var result = queryUsers(params);
+		res.json(result);
+	}
 };
