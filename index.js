03430dc753430d3a2d4d8dfa6cfedeb3e515dbeb
Denis Wolf <dear.mr.dw@gmail.com>
2015-03-24 22:08:55 +0100

    standard express app + .env with the port number

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var data = require('./find_users');
+
+app
+  .get('/', data);
+
+var server = app.listen(process.env.PORT, function() {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
